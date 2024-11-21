ddbbf2c541fdb4002cfa12ca7f685d2ca9ab83e3
ssnlabs <137744428+ssnlabs@users.noreply.github.com>
2024-11-21 22:20:14 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,13 @@
+public class add {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
